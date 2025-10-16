f81c5c9d738992e9c4c9d4ed4bb105f2bbca2518
Victor V. Vinokurov <vit@trs.sut.ru>
2025-10-16 16:28:37 +0300

    Add C functions to work with structs.

--- src/cpp/libpiper.hpp
+++ src/cpp/libpiper.hpp
@@ -11,6 +11,7 @@ using namespace piper;
 #endif
 
 extern "C" {
+	/* Callbacks definition */
 	typedef void (*AudioCallback)(int16_t* audioBuffer, int length);
 	typedef void (*ProgressCallback)(uint16_t progress, size_t total);
 
@@ -24,9 +25,56 @@ extern "C" {
 
 	PIPER_API void setLogLevel(int logLevel);
 
+	/* Working with structs functions */
+	PIPER_API PiperConfig* createPiperConfig(char* eSpeakDataPath) {
+		auto config = new PiperConfig();
+		config->eSpeakDataPath = eSpeakDataPath;
+		return config;
+	}
+	PIPER_API void destroyPiperConfig(PiperConfig* config) {
+		delete config;
+	}
+
+	PIPER_API Voice* createVoice() {
+		return new Voice();
+	}
+	PIPER_API void destroyVoice(Voice* voice) {
+		delete voice;
+	}
+
+	PIPER_API SynthesisResult* createSynthesisResult() {
+		return new SynthesisResult();
+	}
+	PIPER_API void destroySynthesisResult(SynthesisResult* config) {
+		delete config;
+	}
+
+	/* Change SynthesisResult fields */
+	PIPER_API void setSynthesisConfigNoiseScale(Voice* voice, float value) {
+		voice->synthesisConfig.noiseScale = value;
+	}
+
+	PIPER_API void setSynthesisConfigLengthScale(Voice* voice, float value) {
+		voice->synthesisConfig.lengthScale = value;
+	}
+
+	PIPER_API void setSynthesisConfigNoiseW(Voice* voice, float value) {
+		voice->synthesisConfig.noiseW = value;
+	}
+
+	PIPER_API void setSynthesisConfigSentenceSilence(Voice* voice, float value) {
+		voice->synthesisConfig.sentenceSilenceSeconds = value;
+	}
+
+	PIPER_API void setSynthesisConfigSpeakerId(Voice* voice, SpeakerId speakerId) {
+		voice->synthesisConfig.speakerId = speakerId;
+	}
+
+	/* Init/Destroy functions */
 	PIPER_API void initializePiper(PiperConfig* config);
 	PIPER_API void terminatePiper(PiperConfig* config);
 
+	/* Main library API */
 	PIPER_API void loadVoice(PiperConfig* config, const char* modelPath, const char* modelConfigPath, Voice* voice, SpeakerId* speakerId);
 	PIPER_API void textToAudio(PiperConfig* config, Voice* voice, const char* text, SynthesisResult* result, AudioCallback audioCallback, ProgressCallback progressCallback);
 	PIPER_API void textToWavFile(PiperConfig* config, Voice* voice, const char* text, const char* audioFile, SynthesisResult* result, ProgressCallback progressCallback);
